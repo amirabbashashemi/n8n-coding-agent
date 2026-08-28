9ffc36dad4e2c06f6957951510d0433774b12351
amirabbashashemi <89267096+amirabbashashemi@users.noreply.github.com>
2026-08-28 19:40:33 +0330

    ایجاد کنترلر InvoiceController برای مدیریت API های فاکتور.

---
+++ src/main/java/com/example/goldshop/controller/InvoiceController.java
@@ -0,0 +1,10 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/invoices")
+public class InvoiceController {
+    @Autowired
+    private InvoiceService invoiceService;
+}
