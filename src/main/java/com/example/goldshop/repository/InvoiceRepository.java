7b64d625d59de9c6c207d4f1dfd36c76961e4d02
amirabbashashemi <89267096+amirabbashashemi@users.noreply.github.com>
2026-08-28 20:48:01 +0330

    Delete src/main/java/com/example/goldshop directory
    del dir

--- src/main/java/com/example/goldshop/repository/InvoiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.goldshop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.goldshop.model.Invoice;
-
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-    // اینجا می‌توانید متدهای خاص‌تری را برای کار با فاکتورها اضافه کنید.
-}
