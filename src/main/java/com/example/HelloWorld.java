fc59b744880e4d2e09c09378a2e7b8f9313f9995
amirabbashashemi <89267096+amirabbashashemi@users.noreply.github.com>
2026-08-29 23:49:17 +0330

    update HelloWorld class to correct implementation

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hi My Code!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hi My Code!");
+    }
 }
