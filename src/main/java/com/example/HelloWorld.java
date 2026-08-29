7b2935e76f8d2507a4a72fb27e6e85745669e52e
amirabbashashemi <89267096+amirabbashashemi@users.noreply.github.com>
2026-08-30 01:55:41 +0330

    Update pom.xml with Java version 21

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,7 +1,5 @@
-package com.example;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hi My Code!");
-    }
-}
+<properties>
+    <maven.compiler.source>21</maven.compiler.source>
+    <maven.compiler.target>21</maven.compiler.target>
+    <maven.compiler.release>21</maven.compiler.release>
+</properties>
