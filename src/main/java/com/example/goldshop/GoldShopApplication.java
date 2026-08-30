2dd4aefa1eed52cf460e26020c8dc08f76885745
amirabbashashemi <89267096+amirabbashashemi@users.noreply.github.com>
2026-08-31 01:51:05 +0330

    Create GoldShopApplication class with main method and SpringBootApplication annotation.

---
+++ src/main/java/com/example/goldshop/GoldShopApplication.java
@@ -0,0 +1,11 @@
+package com.example.goldshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GoldShopApplication.class, args);
+    }
+}
