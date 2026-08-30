93f0afe467bd64494bb1afde84fbd127a3bc33fb
amirabbashashemi <89267096+amirabbashashemi@users.noreply.github.com>
2026-08-30 12:26:20 +0330

    ایجاد pom.xml برای پیکربندی پروژه

---
+++ src/main/java/com/example/calculator/StringCalculator.java
@@ -0,0 +1,11 @@
+<project xmlns="http://maven.apache.org/POM/4.0.0">
+    <modelVersion>4.0.0</modelVersion>
+    <groupId>com.example</groupId>
+    <artifactId>calculator</artifactId>
+    <version>1.0-SNAPSHOT</version>
+    <properties>
+        <maven.compiler.source>21</maven.compiler.source>
+        <maven.compiler.target>21</maven.compiler.target>
+        <maven.compiler.release>21</maven.compiler.release>
+    </properties>
+</project>
